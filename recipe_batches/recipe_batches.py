96f706ce83b248e2aeb3b411b5d97cea77ddb9df
Smodi <stevesmodish@gmail.com>
2019-02-20 18:05:23 -0500

    switched to vs code for debugging, recipe_batches.py complete

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,12 +3,39 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+    # start a counter
+    num_batches = -1 #My loop will add one during a fail condition
+    enough_ingredients = True
+    # while amount of ingredients is greater than ingredients necessary for the recipe
+    while (enough_ingredients):
+        for key, value in recipe.items():
+            # Subtract the recipe amounts from the on hand amounts
+            if ingredients.get(key):
+                if ingredients[key] - value >= 0:
+                    ingredients[key] = ingredients[key] - value
+                else:
+                    enough_ingredients = False
+        # if any of those ends up being less than 0 -> BREAK
+            else:
+                enough_ingredients = False
+        # otherwise add one to the counter
+        num_batches += 1
+
+    for key, value in ingredients.items():
+        print(key, value)
+    # return the counter number
+    return num_batches
 
 
-if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+
+
+# recipe = { 'milk': 2 }
+# ingredients = { 'milk': 200 }
+# print(recipe_batches(recipe, ingredients))
+
+# if __name__ == '__main__':
+#     # Change the entries of these dictionaries to test
+#     # your implementation with different inputs
+#     recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+#     ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+#     print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
