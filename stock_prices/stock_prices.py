d66f7ba394a89d8362aae32413e7c6ae9a20de94
Smodi <stevesmodish@gmail.com>
2019-02-20 15:21:30 -0500

    stock_prices.py complete but fails test. Another English language issue I believe because 0 is definitely greater than -10.

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,13 +3,35 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+    # start a marker equal to the first price
+    low_marker = prices[0]
+    # start a record of the largest price difference
+    record_difference = 0
 
+    # for every number in prices
+    for i in range(0, len(prices)):
+        # compare the amount of the marker number to the iterator number
+        # if the iterator number is lower than the marker number
+        # move the marker
+        if low_marker > prices[i]:
+            low_marker = prices[i]
+        else:
+            # if it is higher than the marker number
+            # measure the difference between the iterator number and the marker number
+            # if that number is larger than the record replace the record
+            if record_difference < prices[i] - low_marker:
+                record_difference = prices[i] - low_marker
+    # continue doing this until all prices have been checked
+
+    # return the record number
+    return record_difference
+
+print(find_max_profit([1050, 270, 1540, 3800, 2]))
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
