029c70a6c52b78fec9751286ffd5f7e7ff1a0f9c
Smodi <stevesmodish@gmail.com>
2019-02-20 19:19:11 -0500

    climbing stairs seems to work for smaller numbers, working on cache now

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -2,13 +2,28 @@
 
 import sys
 
+cache = {}
+
+
 def climbing_stairs(n, cache=None):
-  pass 
+    if n <= 1:
+        return 1
+    if n == 2:
+        return 2
+    if n in cache:
+        return cache[n]
+    val = climbing_stairs(n - 1) + climbing_stairs(n - 2) + climbing_stairs(n - 3)
+    cache[n] = val
+    return val
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_stairs = int(sys.argv[1])
-    print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
-  else:
-    print('Usage: climbing_stairs.py [num_stairs]')
+    if len(sys.argv) > 1:
+        num_stairs = int(sys.argv[1], cache)
+        print(
+            "There are {ways} ways for a child to jump {n} stairs.".format(
+                ways=climbing_stairs(num_stairs), n=num_stairs
+            )
+        )
+    else:
+        print("Usage: climbing_stairs.py [num_stairs]")
